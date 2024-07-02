615a28f9c036bf8052e6ccb638680bf5bfc116b8
Ahmad Faroun <ahmad996cyc@gmail.com>
2024-07-02 21:38:57 +0200

    fixed a bug with the resolve of the folder creation

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -13,8 +13,8 @@ const createProductFolder = async (uploadPath, files, product, lastProduct) => {
                 const images = await saveImagesToFolder(files, uploadPath);
                 product.image = JSON.stringify(images);
                 await product.updateById(lastProduct);
-                resolve(product);
             }
+            resolve(product);
         });
     });
 };
