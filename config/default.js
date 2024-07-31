3f6a8199a13f3bce1b0f7460d02872baecf4f355
Ahmad Faroun <ahmad996cyc@gmail.com>
2024-07-31 20:05:08 +0200

    Renamed the database

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ module.exports = {
     // DB_PORT: process.env.DB_PORT || '80',
     DB_USERNAME: process.env.DB_USERNAME || 'root',
     DB_USERNAME_PASSWORD: process.env.DB_USERNAME_PASSWORD || '',
-    DB_NAME: process.env.DB_NAME || 'ecommerce_2',
+    DB_NAME: process.env.DB_NAME || 'ecommerce',
     EMAIL_HOST: process.env.EMAIL_HOST,
     EMAIL: process.env.EMAIL,
     EMAIL_PASS: process.env.EMAIL_PASS,
