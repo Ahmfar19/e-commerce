f91bf9cc2e8745cb5ae046ab6fae583a10d23792
Ahmad Faroun <ahmad996cyc@gmail.com>
2024-08-19 16:44:43 +0200

    make an https server when dev

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const dotenv = require('dotenv');
 const http = require('http');
+const https = require('https');
 const app = require('./app');
 const WebSocketManager = require('./websocketManager');
 const WebSocket = require('ws');
+const fs = require('fs');
+const path = require('path');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 if (NODE_ENV === 'development') dotenv.config();
@@ -13,7 +16,16 @@ const config = require('config');
 const PORT = isDevelopment ? 4000 : config.get('PORT');
 
 // Create an HTTP server with the Express app
-const server = http.createServer(app);
+// const server = http.createServer(app);
+let server = http.createServer(app);
+
+if (isDevelopment) {
+    // Läs SSL-certifikatet och nyckeln
+    const key = fs.readFileSync(path.join(__dirname, '/ssl/node/key.pem'), 'utf8');
+    const cert = fs.readFileSync(path.join(__dirname, '/ssl/node/cert.pem'), 'utf8');
+    const options = { key: key, cert: cert };
+    server = https.createServer(options, app);
+}
 
 const wss = new WebSocket.Server({ server });
 
