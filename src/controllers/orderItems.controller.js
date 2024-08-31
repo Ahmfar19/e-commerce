162e8d9ce529fba82baa624126e69c4aab12233a
osamafaroun1 <osamafaroun7@gmail.com>
2024-09-01 00:44:46 +0300

    cleaning code

--- src/controllers/orderItems.controller.js
+++ src/controllers/orderItems.controller.js
@@ -1,14 +1,29 @@
 const OrderItems = require('../models/orderItems.model');
 const { sendResponse } = require('../helpers/apiResponse');
+const Product = require('../models/product.model');
+const Order = require('../models/order.model');
 
 const addProductItems = async (req, res) => {
     try {
-        await OrderItems.saveMulti(req.body);
+        const { products } = req.body;
+
+        const { success, message, insufficientProducts } = await Product.checkQuantities(products);
+
+        if (!success) {
+            return sendResponse(res, 400, 'Bad Request', message, null, insufficientProducts);
+        }
+
+        await Promise.all([
+            OrderItems.saveMulti(req.body),
+            Order.updateProductQuantities(products),
+        ]);
+
+        return sendResponse(res, 201, 'Created', 'Successfully created items.', null, null);
     } catch (error) {
-        sendResponse(res, 500, 'Internal Server Error', null, error.message || error, null);
+        console.error('Error in addProductItems:', error);
+        return sendResponse(res, 500, 'Internal Server Error', 'An unexpected error occurred.', null, null);
     }
 };
-
 const getOrderItems = async (req, res) => {
     const id = req.params.id;
     try {
